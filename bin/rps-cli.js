ad1c84299bed944ccd71635315af46e7c64d3a40
Nathan <nathann@email.unc.edu>
2023-04-25 01:05:18 -0400

    Finished help message for node rps.

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,21 @@
 #!/usr/bin/env node
+import minimist from 'minimist'
+import { rps } from "../lib/rpsls.js"
+import { rpsls } from "../lib/rpsls.js"
+const args = minimist(process.argv.slice(2));
+if (args.h || args.help) {
+    console.log(`Usage: node-rps [SHOT]\n
+Play Rock Paper Scissors (RPS)\n
+\n
+  -h, --help      display this help message and exit\n
+  -r, --rules     display the rules and exit\n
+\n
+Examples:\n
+  node-rps        Return JSON with single player RPS result.\n
+                  e.g. {"player":"rock"}\n
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+process.exit(0); }
+if (args._) {
+    rps(args._);
+}
